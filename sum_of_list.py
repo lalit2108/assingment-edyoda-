03da81c5e27118478c78b009b281706c11ac16ed
lalit2108 <131739145+lalit2108@users.noreply.github.com>
2023-05-20 10:54:00 +0530

    Add files via upload

---
+++ sum_of_list.py
@@ -0,0 +1,11 @@
+lst = [8,2,3,0,7]
+sum = 0
+for L in lst :
+    sum = L + sum 
+print(sum)
+
+# explanation :-
+# The given sample list is (8, 2, 3, 0, 7), and the expected output is 20.
+# To calculate the output, we need to sum all the numbers in the list.
+# 8 + 2 + 3 + 0 + 7 = 20
+# Therefore, the expected output is 20.
